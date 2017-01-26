b45a9347610a8f12ff81ccf2720381e33c7b6566
Antonio Fiol <antonio@fiol.es>
2017-01-26 07:44:41 +0100

    Remove unused files. Add license for webcolors.

--- sol.py
+++
@@ -1,9 +0,0 @@
-from tira import *
-import time
-
-t = Tira(i2cdebug=True, num_leds=180, dobleces=1, invirtiendo=True)
-for estilo in [ "", "-gris50", "-color" ]:
-  for im in [ "sol20", "sol40", "sol60" ]:
-    for x in range(100):
-      t.poner_imagen("sol/"+im+estilo+".jpg", 1, x)
-      time.sleep(.2)
